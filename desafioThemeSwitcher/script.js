51de53d5304736203fad42db2870d8478790db0e
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-10-21 14:05:25 -0300

    feat: desafio ThemeSwitcher

---
+++ desafioThemeSwitcher/script.js
@@ -0,0 +1,8 @@
+ball = document.querySelector("#ball");
+ballh1 = document.querySelector(".ballh1");
+background = document.querySelector(".container")
+
+ball.onclick = () => {
+    background.classList.toggle("active");
+    ballh1.classList.toggle("active");
+}
